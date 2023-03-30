de8bde228dc4a40cd81a079c34bde94b109362a6
grant <gstoltman@gmail.com>
2023-03-29 18:40:37 -0700

    Updates to code but still not functioning

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import requests
-import os
+import shutil
 
 url = input('Enter url: ')
-folder_path = r"C:/Users/Grant/Desktop/backgrounds"
 
-response = requests.get(url)
+folder_path = "home/gstoltman/pictures/wallpapers/image.jpg"
 
-if response.status_code == 200:
-    with open(os.path.join(folder_path, "image.jpg"), "wb") as f:
-        f.write(response.content)
-        print("Image saved to:", os.path.join(folder_path, "image.jpg"))
-else:
-    print("Failed to download image")
+response = requests.get(url, stream=True)
+
+with open(url, "wb") as f:
+    response.raw.decode_content = True
+    shutil.copyfileobj(response.raw, f)
+    print("Image saved to:", folder_path)
