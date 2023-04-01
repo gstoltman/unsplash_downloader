09c1e380645c78addf0322aa5df248491eea19bb
grant <gstoltman@gmail.com>
2023-03-31 18:52:37 -0700

    Closer, need to make webbrowser work

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import requests
-import shutil
+import os
+import webbrowser
 
-url = input('Enter url: ')
 
-folder_path = "home/gstoltman/pictures/wallpapers/image.jpg"
+firefox_path = "/mnt/c/'Program Files'/'Mozilla Firefox'/firefox.exe %s"
+webbrowser.register('firefox', None, firefox_path)
 
-response = requests.get(url, stream=True)
+tab_url = webbrowser.get('firefox').tab.url
 
-with open(url, "wb") as f:
-    response.raw.decode_content = True
-    shutil.copyfileobj(response.raw, f)
-    print("Image saved to:", folder_path)
+print(tab_url)
+
+# Set the directory where you want to save the image
+#directory = '/home/gstoltman/pictures/wallpapers/'
+
+# Send a GET request to the URL to download the image
+#response = requests.get(url)
+
+# Extract the filename from the URL
+#filename = 'image.jpg'
+
+# Join the directory and filename to create the full file path
+#filepath = os.path.join(directory, filename)
+
+# Write the image to the file
+#with open(filepath, 'wb') as f:
+#    f.write(response.content)
+
+#print(f'Successfully downloaded and saved {filename} to {directory}.')
